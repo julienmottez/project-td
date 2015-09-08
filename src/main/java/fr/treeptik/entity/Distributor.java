bf2027f9c415e8ea2e6f3194c2988a76b7d19882
mryapados <ced.sevestre@gmail.com>
2015-09-08 12:29:47 +0200

    Update Distributor.java

--- src/main/java/fr/treeptik/entity/Distributor.java
+++ src/main/java/fr/treeptik/entity/Distributor.java
@@ -10,6 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
+@Table(name="distributor")
 public class Distributor implements Serializable{
 
 	private static final long serialVersionUID = 1L;
