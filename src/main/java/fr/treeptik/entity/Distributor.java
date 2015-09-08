706a00f9ae38fad90a9fff727ed6ae9cddac0fcb
mak <bmakhlouf@hotmail.fr>
2015-09-08 11:20:08 +0200

    Add Distributor entity, dao.

--- src/main/java/fr/treeptik/entity/Distributor.java
+++ src/main/java/fr/treeptik/entity/Distributor.java
@@ -2,31 +2,32 @@ package fr.treeptik.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Distributor implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private Integer id;
-
+	@Embedded
+	private Address address;
+	
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
-	
-	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 }
