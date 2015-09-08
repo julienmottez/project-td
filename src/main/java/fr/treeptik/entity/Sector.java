115d34822f3d9e7085402de655cbe58b159b9f95
faten <faten.moualia@yahoo.fr>
2015-09-08 11:52:58 +0200

    modify sector.java et ajouter Person.java

--- src/main/java/fr/treeptik/entity/Sector.java
+++ src/main/java/fr/treeptik/entity/Sector.java
@@ -1,10 +1,15 @@
 package fr.treeptik.entity;
 
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 
@@ -13,19 +18,53 @@ public class Sector {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	private String name;
+	private Person managerSector;
+	
+	@OneToOne
+	private Area area;
+	
+	public Person getManagerSector() {
+		return managerSector;
+	}
+	public void setManagerSector(Person managerSector) {
+		this.managerSector = managerSector;
+	}
 	public Integer getId() {
 		return id;
 	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	@Column(name = "name")
+	private String name;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Distributor>distributors;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<DistributionPoint>distributionPoints;
+
+	public List<Distributor> getDistributors() {
+		return distributors;
+	}
+	public void setDistributors(List<Distributor> distributors) {
+		this.distributors = distributors;
+	}
+	public List<DistributionPoint> getDistributionPoints() {
+		return distributionPoints;
+	}
+	public void setDistributionPoints(List<DistributionPoint> distributionPoints) {
+		this.distributionPoints = distributionPoints;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
