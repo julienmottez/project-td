5da73cbb5681ef711092ccb0cbb10aa1b5a85de0
mryapados <ced.sevestre@gmail.com>
2015-09-08 12:32:28 +0200

    import distributor

--- src/main/java/fr/treeptik/entity/Distributor.java
+++ src/main/java/fr/treeptik/entity/Distributor.java
@@ -1,13 +1,16 @@
 package fr.treeptik.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="distributor")
