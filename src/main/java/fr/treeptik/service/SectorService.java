5effe73b4b28f0ca19d49f228f9a65625f77fefc
faten <faten.moualia@yahoo.fr>
2015-09-08 11:58:55 +0200

    modify sectorService

--- src/main/java/fr/treeptik/service/SectorService.java
+++ src/main/java/fr/treeptik/service/SectorService.java
@@ -12,51 +12,50 @@ import fr.treeptik.exception.ServiceException;
 @Service
 @Scope(value = "singleton")
 public class SectorService {
-private Logger logger = Logger.getLogger(SectorService.class);
-@Autowired
-
-private SectorDAO sectorDAO;
-@Transactional()
-public Sector save(Sector sector) throws ServiceException {
-logger.debug("appel de la methode save sector " + sector.getName());
-try {
-return sectorDAO.save(sector);
-} catch (PersistenceException e) {
-logger.error("erreur save personne " + e.getMessage());
-throw new ServiceException("erreur save sector", e);
-}
-}
-@Transactional
-public Sector update(Sector sector) throws ServiceException {
-logger.debug("appel de la methode update sector " + sector.getName());
-try {
-return sectorDAO.save(sector);
-} catch (PersistenceException e) {
-logger.error("erreur update personne " + e.getMessage());
-throw new ServiceException("erreur update sector", e);
-}
-}
-public List<Sector> findAll() throws ServiceException {
-try {
-return sectorDAO.findAll();
-} catch (PersistenceException e) {
-throw new ServiceException("erreur findAll sector", e);
-}
-}
-public Sector findById(Integer id) throws ServiceException {
-try {
-return sectorDAO.findOne(id);
-} catch (PersistenceException e) {
-throw new ServiceException("erreur findById sector", e);
-}
-}
-@Transactional
-public void deleteSector(Sector sector) throws ServiceException {
-try {
-sectorDAO.delete(sector.getId());
-} catch (PersistenceException e) {
-throw new ServiceException("erreur save sector", e);
-}
-}
+	private Logger logger = Logger.getLogger(SectorService.class);
+	@Autowired
+	private SectorDAO sectorDAO;
+	@Transactional()
+	public Sector save(Sector sector) throws ServiceException {
+		logger.debug("appel de la methode save sector " + sector.getName());
+		try {
+			return sectorDAO.save(sector);
+		} catch (PersistenceException e) {
+			logger.error("erreur save personne " + e.getMessage());
+			throw new ServiceException("erreur save sector", e);
+		}
+	}
+	@Transactional
+	public Sector update(Sector sector) throws ServiceException {
+		logger.debug("appel de la methode update sector " + sector.getName());
+		try {
+			return sectorDAO.save(sector);
+		} catch (PersistenceException e) {
+			logger.error("erreur update personne " + e.getMessage());
+			throw new ServiceException("erreur update sector", e);
+		}
+	}
+	public List<Sector> findAll() throws ServiceException {
+		try {
+			return sectorDAO.findAll();
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur findAll sector", e);
+		}
+	}
+	public Sector findById(Integer id) throws ServiceException {
+		try {
+			return sectorDAO.findOne(id);
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur findById sector", e);
+		}
+	}
+	@Transactional
+	public void deleteSector(Sector sector) throws ServiceException {
+		try {
+			sectorDAO.delete(sector.getId());
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur save sector", e);
+		}
+	}
 
 }
