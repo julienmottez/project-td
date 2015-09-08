b0abddcb9117e9fb80ad5c1a2e411060e2089ec4
tralptexk6 <trptkalex6@gmail.com>
2015-09-08 16:45:19 +0200

    repository annotations missions

--- src/main/java/fr/treeptik/dao/PersonDAO.java
+++ src/main/java/fr/treeptik/dao/PersonDAO.java
@@ -1,9 +1,11 @@
 package fr.treeptik.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.treeptik.entity.Person;
 
+@Repository
 public interface PersonDAO extends JpaRepository<Person, Integer> {
 
 }
