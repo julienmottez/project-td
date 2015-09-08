9e0f491d15a67ad179365444c288ffc3d0d1d6f8
stagiaire <stagiaire@Stagiaire06>
2015-09-08 10:31:11 +0200

    commit hakim tecnhician dao service

---
+++ td/src/main/java/fr/treeptik/dao/TechnicianDAO.java
@@ -0,0 +1,12 @@
+package fr.treeptik.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.treeptik.entity.Technician;
+
+@Repository
+public interface TechnicianDAO extends JpaRepository<Technician, Integer> {
+	
+	
+}
---
+++ td/src/main/java/fr/treeptik/service/TechnicianService.java
@@ -0,0 +1,76 @@
+package fr.treeptik.service;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.treeptik.dao.TechnicianDAO;
+import fr.treeptik.entity.Technician;
+import fr.treeptik.exception.ServiceException;
+
+@Service
+@Scope(value = "singleton")
+public class TechnicianService {
+
+	private Logger logger = Logger.getLogger(TechnicianService.class);
+
+	@Autowired
+	//@Qualifier("playerDAO")
+	private TechnicianDAO technicianDAO;
+
+	@Transactional()
+	public Technician save(Technician technician) throws ServiceException {
+		logger.debug("appel de la methode save technician " + technician.getName());
+
+		try {
+			return technicianDAO.save(technician);
+		} catch (PersistenceException e) {
+			logger.error("erreur save personne " + e.getMessage());
+			throw new ServiceException("erreur save technician", e);
+		}
+	}
+
+	@Transactional
+	public Technician update(Technician technician) throws ServiceException {
+		logger.debug("appel de la methode update technician " + technician.getName());
+		try {
+			return technicianDAO.save(technician);
+		} catch (PersistenceException e) {
+			logger.error("erreur update personne " + e.getMessage());
+			throw new ServiceException("erreur update technician", e);
+		}
+	}
+
+	public List<Technician> findAll() throws ServiceException {
+		try {
+			return technicianDAO.findAll();
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur findAll technician", e);
+		}
+	}
+
+	public Technician findById(Integer id) throws ServiceException {
+		try {
+			return technicianDAO.findOne(id);
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur findById technician", e);
+		}
+	}
+
+	@Transactional
+	public void deleteTechnician(Technician technician) throws ServiceException {
+		try {
+			technicianDAO.delete(technician.getId());
+		} catch (PersistenceException e) {
+			throw new ServiceException("erreur save technician", e);
+		}
+	}
+	
+	
+}
