4c8f26e674dbe1f06f53444ceb7d4b104a2a540c
tralptexk6 <trptkalex6@gmail.com>
2015-09-08 14:18:42 +0200

    DrinkService test

---
+++ src/test/java/fr/treeptik/service/DrinkServiceTest.java
@@ -0,0 +1,29 @@
+package fr.treeptik.service;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.treeptik.dao.DrinkDao;
+
+public class DrinkServiceTest {
+
+	private static final int ENTITY_ID = 999_999;
+	
+	private DrinkService drinkService;
+	private DrinkDao mockDrinkDao;
+	
+	@Before
+	public void setUp() {
+		mockDrinkDao = mock(DrinkDao.class);
+		drinkService = new DrinkService(mockDrinkDao);
+	}
+	
+	@Test
+	public void testFindById() throws Exception {
+		drinkService.getById(ENTITY_ID);
+		
+		verify(mockDrinkDao).findById(ENTITY_ID);
+	}
+}
