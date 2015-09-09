54de34ac1e8e5ceb6f83d5855f47e768aea85207
mryapados <ced.sevestre@gmail.com>
2015-09-09 15:02:03 +0200

    person

--- src/main/java/fr/treeptik/entity/Person.java
+++ src/main/java/fr/treeptik/entity/Person.java
@@ -40,9 +40,6 @@ public class Person implements Serializable {
 	@Embedded
 	private Address adress;
 
-	public Person() {
-	}
-
 	public Integer getId() {
 		return id;
 	}
@@ -66,7 +63,7 @@ public class Person implements Serializable {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
@@ -81,10 +78,6 @@ public class Person implements Serializable {
 
 	public void setEncryptedPassword(String encryptedPassword) {
 		this.encryptedPassword = encryptedPassword;
-	}
-
-	public Address getAdress() {
-		return adress;
 	}
 
 	public Boolean getEnabled() {
@@ -103,51 +96,15 @@ public class Person implements Serializable {
 		this.role = role;
 	}
 
+	public Address getAdress() {
+		return adress;
+	}
+
 	public void setAdress(Address adress) {
 		this.adress = adress;
 	}
 
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		return true;
-	}
+	
 
 	
 }
