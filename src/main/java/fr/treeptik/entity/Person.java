95738369f6ca8da85b127d7559f58116794a2832
fatima <fatima.ksabi@gmail.com>
2015-09-09 15:38:14 +0200

    maj person

--- src/main/java/fr/treeptik/entity/Person.java
+++ src/main/java/fr/treeptik/entity/Person.java
@@ -3,16 +3,25 @@ package fr.treeptik.entity;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name="person")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+@DiscriminatorValue("Person")
+
+
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
