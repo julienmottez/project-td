5a51487f83e035d63423d1c5cb0485a0b5711cd8
fatima <fatima.ksabi@gmail.com>
2015-09-09 15:31:47 +0200

    -

--- src/main/java/fr/treeptik/entity/Person.java
+++ src/main/java/fr/treeptik/entity/Person.java
@@ -2,6 +2,7 @@ package fr.treeptik.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,17 +21,24 @@ public class Person implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
-
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
 	private String login;
+	
+	@Column(name = "encrypted_password")
 	private String encryptedPassword;
+	
+	@Column(name = "enabled")
+	private Boolean enabled;
+	
+	@Column(name = "role")
+	private String role;
 	
 	@Embedded
 	private Address adress;
-
-	public Person() {
-	}
 
 	public Integer getId() {
 		return id;
@@ -55,7 +63,7 @@ public class Person implements Serializable {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
@@ -72,6 +80,22 @@ public class Person implements Serializable {
 		this.encryptedPassword = encryptedPassword;
 	}
 
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 	public Address getAdress() {
 		return adress;
 	}
@@ -80,47 +104,7 @@ public class Person implements Serializable {
 		this.adress = adress;
 	}
 
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		return true;
-	}
+	
 
 	
 }
