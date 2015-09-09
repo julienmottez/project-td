d250ebaa6b365fc423c3c3249773a9a52d5b5020
mryapados <ced.sevestre@gmail.com>
2015-09-09 09:45:13 +0200

    fix application conf

--- src/main/java/fr/treeptik/conf/ApplicationConfiguration.java
+++ src/main/java/fr/treeptik/conf/ApplicationConfiguration.java
@@ -88,7 +88,7 @@ public class ApplicationConfiguration {
 	public UrlBasedViewResolver urlBasedViewResolver() {
 		UrlBasedViewResolver res = new InternalResourceViewResolver();
 		res.setViewClass(JstlView.class);
-		res.setPrefix("/WEB-INF/pages/");
+		res.setPrefix("/pages/");
 		res.setSuffix(".jsp");
 
 		return res;
