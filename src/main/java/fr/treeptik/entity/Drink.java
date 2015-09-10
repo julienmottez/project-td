486c7ecf8e38763b08e816171509a7822ab9f923
tralptexk6 <trptkalex6@gmail.com>
2015-09-10 10:13:34 +0200

    updated entity Drink
    	- removed unused association with distribution point
    	- added toString and equals/hashCode
    	- added a name

--- src/main/java/fr/treeptik/entity/Drink.java
+++ src/main/java/fr/treeptik/entity/Drink.java
@@ -1,7 +1,6 @@
 package fr.treeptik.entity;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -9,10 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,22 +17,18 @@ public class Drink implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @Column(name = "cold")
-    @Embedded
-    private TemperatureRange coldStorage;
-    
-    @ManyToMany
-    @JoinTable(name="distributionpoint_drink", 
-    joinColumns=@JoinColumn(name="distributionpoint_id"),
-    inverseJoinColumns=@JoinColumn(name="drink_id"))
-    private List<DistributionPoint> distributionPoint;
+	private String name;
 
-    public Drink() {
+	@Column(name = "cold")
+	@Embedded
+	private TemperatureRange coldStorage;
+
+	public Drink() {
 	}
-    
+
 	public Drink(int id, TemperatureRange coldStorage) {
 		this.id = id;
 		this.coldStorage = coldStorage;
@@ -59,11 +50,42 @@ public class Drink implements Serializable {
 		this.coldStorage = coldStorage;
 	}
 
-	public List<DistributionPoint> getDistributionPoint() {
-		return distributionPoint;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coldStorage == null) ? 0 : coldStorage.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
 
-	public void setDistributionPoint(List<DistributionPoint> distributionPoint) {
-		this.distributionPoint = distributionPoint;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Drink other = (Drink) obj;
+		if (coldStorage == null) {
+			if (other.coldStorage != null)
+				return false;
+		} else if (!coldStorage.equals(other.coldStorage))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return name;
 	}
 }
