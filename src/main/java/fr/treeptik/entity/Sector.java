3cdef723a9dada616bbde052cac70541ecca8fa3
faten <faten.moualia@yahoo.fr>
2015-09-10 09:34:31 +0200

    modify sector person

--- src/main/java/fr/treeptik/entity/Sector.java
+++ src/main/java/fr/treeptik/entity/Sector.java
@@ -9,6 +9,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -22,7 +23,7 @@ public class Sector {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
-	@OneToOne
+	@ManyToOne
 	private SectorManager managerSector;
 
 	@OneToOne
