d33f4249b387aba6c01e02ca36f5315e7c36d5ca
faten <faten.moualia@yahoo.fr>
2015-09-12 17:49:41 +0200

    maj technician

--- src/main/java/fr/treeptik/controller/TechnicianController.java
+++ src/main/java/fr/treeptik/controller/TechnicianController.java
@@ -1,12 +1,20 @@
 package fr.treeptik.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.treeptik.entity.Distributor;
 import fr.treeptik.entity.Technician;
 import fr.treeptik.exception.ServiceException;
 import fr.treeptik.service.DistributorService;
@@ -26,7 +34,7 @@ public class TechnicianController {
 	@Autowired
 	private DistributorService distributorService;
 	
-
+	private Map<String, Distributor> distributorCache;
 
 
 	@RequestMapping(value = "/new.html", method = RequestMethod.GET)
@@ -35,7 +43,13 @@ public class TechnicianController {
 		ModelAndView modelAndView = new ModelAndView("admin/technician/technician");
 		modelAndView.addObject("technician", new Technician());
 		modelAndView.addObject("sectors", sectorService.findAll());
-		modelAndView.addObject("distributorss", distributorService.findAll());
+		distributorCache = new HashMap<>();
+		List<Distributor> distributors = distributorService.findAll();
+		for (Distributor distributor : distributors) {
+			distributorCache.put(distributor.getId().toString(), distributor);
+		}
+		modelAndView.addObject("distributors", distributors);
+		modelAndView.addObject("technician", new Technician());
 
 		return modelAndView;
 	}
@@ -70,7 +84,12 @@ public class TechnicianController {
 
 	@RequestMapping(value = "/save.html", method = RequestMethod.POST)
 	public ModelAndView save(Technician technician) throws ServiceException {
-		try {
+           try {
+			
+			for (Distributor distributor : technician.getDistributors()) {
+				System.out.println(distributor.getLabel());
+			}
+			
 			if (technician.getId() == null) {
 				technicianService.save(technician);
 			} else {
@@ -85,6 +104,26 @@ public class TechnicianController {
 		}
 	}
 	
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) throws Exception {
+		binder.registerCustomEditor(List.class, "distributors", new CustomCollectionEditor(List.class) {
+			protected Object convertElement(Object element) {
+				if (element instanceof Distributor) {
+					System.out.println("Converting from Staff to Staff: " + element);
+					return element;
+				}
+				if (element instanceof String) {
+					Distributor distributor = distributorCache.get(element);
+					System.out.println("Looking up staff for id " + element + ": " + distributor);
+					return distributor;
+				}
+				System.out.println("Don't know what to do with: " + element);
+				return null;
+			}
+		});
+	}
+	
 	@RequestMapping(value = "/delete.html", method = RequestMethod.GET)
 	public ModelAndView delete(Technician technician) throws ServiceException {
 		
@@ -93,5 +132,6 @@ public class TechnicianController {
 		modelAndView.addObject("technician", new Technician());
 		return modelAndView;
 	}
-
+	
+	
 }
--- web/WEB-INF/pages/admin/technician/technician.jsp
+++ web/WEB-INF/pages/admin/technician/technician.jsp
@@ -18,24 +18,35 @@
 	<c:if test="${not empty error}">
 		<label style="color: red;"><c:out value="${error}" /></label>
 	</c:if>
-	<form:form action="save.html" commandName="technician" method="POST">
+	<form:form action="save.html" commandName="technician" method="POST" class="form-horizontal">
 		<form:hidden path="id" />
+		
+		<div class="form-group">
 		<label>First Name:</label>
 		<form:input path="firstName" id="nom" />
+		</div>
 	<br />
+	<div class="form-group">
 		<label>Last Name:</label>
 		<form:input path="lastName" id="prenom" />
+		</div>
 	<br />
+	<div class="form-group">
  		<label>Sector:</label>
 
 		<form:select path="sector.id" id="sector">
 			<form:options items="${sectors}" itemValue="id" itemLabel="name" />
 		</form:select>
+		</div>
 	<br />	
-		<label>Distributor :</label>
-		<form:select  path="distributors" id="distributor">
-			<form:options items="${distributorss}" itemValue="id" itemLabel="id" onClick="javascript:OnSelect(this);"/>
-		</form:select>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="distributor">Distributors : </label>
+				<div class="col-sm-6">
+					<form:select size="10" id="distributor" class="form-control" multiple="true" path="distributors">
+					     <form:options items="${distributors}" itemValue="id" itemLabel="label" />
+					</form:select>
+				</div>
+			</div>
 		<br />
 </br>
 </br>
